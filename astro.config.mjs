afb0f6cc7030f24cd7845af7bc2b83d7b06d2355
JoseCruz <jose.crperez@gmail.com>
2024-01-12 10:23:11 -0600

    feat():about me page

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  base: 'https://camas-facenco.github.io/team-profiles/',
+  site: 'https://camas-facenco.github.io/team-profiles/',
   integrations: [tailwind()]
 });
